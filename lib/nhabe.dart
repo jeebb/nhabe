b29783bc29caf42a586c4778f0de0413841a381f
Duy Nguyen <jeenguyen.vn@gmail.com>
2020-08-03 09:13:03 +0700

    Move month picker to root package

--- lib/nhabe.dart
+++ lib/nhabe.dart
@@ -3,12 +3,11 @@ library nhabe;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-part "src/nb_calendar.dart";
+part "src/internal/calendar_header.dart";
+part "src/internal/month_days.dart";
+part "src/internal/week_days.dart";
 part "src/models.dart";
+part 'src/month_picker.dart';
+part "src/nb_calendar.dart";
 part "src/styles.dart";
 part 'src/swiss_knife.dart';
-
-part "src/internal/calendar_header.dart";
-part "src/internal/week_days.dart";
-part "src/internal/month_days.dart";
-part 'src/internal/month_picker.dart';
